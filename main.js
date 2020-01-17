02cb1d096a7e77aff960ced12bb165dd38fffcf7
Ben Farrell <ben@benfarrell.com>
2020-01-17 11:16:04 -0800

    workflow complete-ish

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 const Jimp = require('jimp');
 const fs = require('fs');
+const extra =  require('fs-extra');
+const os = require('os');
 const watch = require('node-watch');
 const path = require('path');
 const alphaAutoCrop = require('./jimp-plugin-alpha-autocrop.js');
 
-const watchfolder = './examplewatchfolder';
+const watchfolder = os.homedir() + '/Desktop/to-process';
+const baseTemplateFolder = os.homedir() + '/Desktop/masks';
+
 watch( watchfolder, { recursive: true }, (eventType, filename) => {
-    if (path.extname(filename) === '.png' && eventType === 'update') {
+    if ((path.extname(filename) === '.png' || path.extname(filename) === '.jpg') && eventType === 'update') {
         if (fs.existsSync(filename)) {
             const subdir = path.relative(watchfolder, filename);
-            const templatename = subdir.split('/')[0];
+            const templatename = subdir.split(path.sep)[0];
+            processImage(templatename, filename);
         }
     }
 });
 
-/*
-const templateFolder = './exampleimages/vincent_price_avatar';
+function processImage(template, filenametoprocess) {
+    const folderName = `${os.homedir()}/Desktop/out/${Date.now()}`;
+    fs.mkdirSync(folderName);
 
-fs.readdir(templateFolder, function (err, filenames) {
-    if (err) {
-        onError(err);
-        return;
-    }
-    filenames.forEach(function (filename) {
-        Jimp.read('./exampleimages/price.jpg', (err, paperdoll) => {
-            paperdoll.alphaAutoCrop = alphaAutoCrop;
+    fs.readdir(baseTemplateFolder + path.sep + template, function (err, maskfilenames) {
+        if (err) {
+            onError(err);
+            return;
+        }
+        maskfilenames.forEach(function (maskfilename) {
+            Jimp.read(filenametoprocess, (err, paperdoll) => {
+                paperdoll.alphaAutoCrop = alphaAutoCrop;
 
-            if (filename.indexOf('.png')) {
-                if (err) throw err;
-
-                const result = Jimp.read(`${templateFolder}/${filename}`, (err, mask) => {
-                    paperdoll
+                if (maskfilename.indexOf('.png') !== -1) {
+                    if (err) throw err;
+                    const result = Jimp.read(`${templateFolder}/${maskfilename}`, (err, mask) => {
+                        paperdoll
                         .mask(mask)
                         .alphaAutoCrop()
-                        .write(`./out/${filename}`); // save
-                });
-            }
+                        .write(`${folderName}/${maskfilename}`); // save
+                    });
+                } else if (maskfilename.indexOf('.json') !== -1) {
+                    extra.copySync(`${baseTemplateFolder}/${template}/${maskfilename}`, `${folderName}/manifest.json` );
+                }
+            });
         });
     });
-});
-*/
+
+}
+
+
+const templateFolder = './exampleimages/vincent_price_avatar';
